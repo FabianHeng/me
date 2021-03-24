85830db3fa10152d79b8dcf45f7e7283b4d7495b
FabianHeng <FireStone@users.noreply.github.com>
2021-03-24 22:29:36 +0800

    added events, sticky nav

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,12 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar');
     const navLinks = document.querySelectorAll('.navbar li')
+
+    //Animate Nav on scroll
+    window.addEventListener("scroll", () => {
+        const nav = document.querySelector("nav");
+        nav.classList.toggle("sticky", window.scrollY > 0);
+    })
     
     // Toggle Nav
     burger.addEventListener('click', () => {
